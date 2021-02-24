8afd6c3bafd7578774a56522934f1307bb572cee
Will <willscullythewag@gmail.com>
2021-02-24 10:15:55 +0000

    Add base manager methods to DamageListManager

--- OofTracker-Spigot/src/main/java/net/cg360/spigot/ooftracker/lists/DamageListManager.java
+++ OofTracker-Spigot/src/main/java/net/cg360/spigot/ooftracker/lists/DamageListManager.java
@@ -1,8 +1,35 @@
 package net.cg360.spigot.ooftracker.lists;
 
+import java.util.HashMap;
+import java.util.UUID;
+
 public class DamageListManager {
 
+    private static DamageListManager primaryManager;
+
+    private HashMap<UUID, DamageList> damageLists;
+
+
+    public DamageListManager() {
+        this.damageLists = new HashMap<>();
+    }
+
+
+    /**
+     * Sets the manager the result provided from KitRegistry#get() and
+     * finalizes the instance to an extent.
+     *
+     * Cannot be changed once initially called.
+     */
+    public void setAsPrimaryManager(){
+        if(primaryManager == null) primaryManager = this;
+    }
 
 
 
+
+    /** @return the primary instance of the EventManager. */
+    public static DamageListManager get(){
+        return primaryManager;
+    }
 }
