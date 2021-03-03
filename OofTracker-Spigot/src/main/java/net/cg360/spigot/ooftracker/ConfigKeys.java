89ba901f8de4f2c51cc358b8416d8ae5f9d2adec
Will <willscullythewag@gmail.com>
2021-03-03 17:42:24 +0000

    Add more config keys

--- OofTracker-Spigot/src/main/java/net/cg360/spigot/ooftracker/ConfigKeys.java
+++ OofTracker-Spigot/src/main/java/net/cg360/spigot/ooftracker/ConfigKeys.java
@@ -11,6 +11,7 @@ public final class ConfigKeys {
     public static final Key<Boolean> DAMAGE_LISTS_ONLY = new Key<>("damage-lists-only"); // default: false
 
 
+
     // -- Damage Lists --
 
     // Should entities which aren't players be tracked?
@@ -18,5 +19,21 @@ public final class ConfigKeys {
 
     // Should a given DamageList be cleared once it's entity dies?
     public static final Key<Boolean> LIST_CLEAR_ON_DEATH = new Key<>("clear-list-on-death"); // default: true
+
+
+
+    // -- Death Messages --
+
+    // Are custom death messages enabled?
+    public static final Key<Boolean> DEATH_MESSAGES_ENABLED = new Key<>("death-messages-enabled");
+
+    // Are the bonus tags at the end of assisted deaths enabled?
+    public static final Key<Boolean> ASSIST_TAGS_ENABLED = new Key<>("assist-tags-enabled"); // default: true
+
+    // Are kill notices enabled?
+    public static final Key<Boolean> KILL_NOTICES_ENABLED = new Key<>("kill-notices-enabled"); // default: false
+
+    // Are assist-kill notices enabled? Only triggered when you are not the final killer.
+    public static final Key<Boolean> ASSIST_KILL_NOTICES_ENABLED = new Key<>("assist-kill-notices-enabled"); // default: true
 
 }
