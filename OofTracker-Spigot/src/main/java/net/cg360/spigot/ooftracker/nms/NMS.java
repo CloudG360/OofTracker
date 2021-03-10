126046d10b306cdcfab923650401e4e47e4e7d27
Will <willscullythewag@gmail.com>
2021-03-10 17:25:08 +0000

    Documentation is pog

--- OofTracker-Spigot/src/main/java/net/cg360/spigot/ooftracker/nms/NMS.java
+++ OofTracker-Spigot/src/main/java/net/cg360/spigot/ooftracker/nms/NMS.java
@@ -9,16 +9,21 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 public class NMS {
 
+    /**
+     * Hooks into the Entity NMS to return the current
+     * value of the static field "entityCount" (Used for entity IDs),
+     * then incrementing it.
+     * @return the value of the static AtomicInteger "entityCount" in the NMS Entity
+     */
     public static int getNewEntityID() {
-        Class<Entity> cls = Entity.class;
+        Class<Entity> cls = Entity.class; // Get NMS Entity class
 
         try {
             Field field = cls.getDeclaredField("entityCount");
             field.setAccessible(true); // Ensure accessible, might as well keep it open.
-            AtomicInteger aI = (AtomicInteger) field.get(null);
+            AtomicInteger aI = (AtomicInteger) field.get(null); // Get static field.
 
-            int value = aI.getAndIncrement();
-            return value;
+            return aI.getAndIncrement(); // Get the ID and then increment it
 
         } catch (NoSuchFieldException err) {
             OofTracker.getLog().severe("Unable to increment entity ID - No field! Is this the wrong version?");
