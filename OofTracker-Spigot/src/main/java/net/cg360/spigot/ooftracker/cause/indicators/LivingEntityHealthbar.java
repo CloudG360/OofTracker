40cba12b8ffb0be614a69eb3a97057ca0bc1fb30
Will <willscullythewag@gmail.com>
2021-03-10 18:44:10 +0000

    Use reflection to set packet properties

--- OofTracker-Spigot/src/main/java/net/cg360/spigot/ooftracker/cause/indicators/LivingEntityHealthbar.java
+++ OofTracker-Spigot/src/main/java/net/cg360/spigot/ooftracker/cause/indicators/LivingEntityHealthbar.java
@@ -4,9 +4,11 @@ import net.cg360.spigot.ooftracker.ConfigKeys;
 import net.cg360.spigot.ooftracker.OofTracker;
 import net.cg360.spigot.ooftracker.Util;
 import net.cg360.spigot.ooftracker.nms.NMS;
+import net.minecraft.server.v1_16_R3.MathHelper;
 import net.minecraft.server.v1_16_R3.PacketPlayOutEntityDestroy;
 import net.minecraft.server.v1_16_R3.PacketPlayOutSpawnEntityLiving;
 import net.minecraft.server.v1_16_R3.Vec3D;
+import org.bukkit.craftbukkit.v1_16_R3.entity.CraftLivingEntity;
 import org.bukkit.craftbukkit.v1_16_R3.entity.CraftPlayer;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.LivingEntity;
@@ -63,7 +65,47 @@ public class LivingEntityHealthbar {
                         CraftPlayer cPlayer = (CraftPlayer) p;
                         PacketPlayOutSpawnEntityLiving addPacket = new PacketPlayOutSpawnEntityLiving(); // An armour stand is apparently living??
 
+                        try {
 
+                            NMS.setClassField(PacketPlayOutSpawnEntityLiving.class, addPacket, "a", fakeEntityID); // Entity ID
+                            NMS.setClassField(PacketPlayOutSpawnEntityLiving.class, addPacket, "b", fakeEntityUUID); // Entity UUID
+                            NMS.setClassField(PacketPlayOutSpawnEntityLiving.class, addPacket, "c", EntityType.ARMOR_STAND); // Entity Type
+
+                            // Copy host entity's location and follow
+                            NMS.setClassField(PacketPlayOutSpawnEntityLiving.class, addPacket, "d", hostEntity.getLocation().getX()); // Location X
+                            NMS.setClassField(PacketPlayOutSpawnEntityLiving.class, addPacket, "e", hostEntity.getLocation().getY()); // Location Y
+                            NMS.setClassField(PacketPlayOutSpawnEntityLiving.class, addPacket, "f", hostEntity.getLocation().getZ()); // Location Z
+
+                            // Copy host entity's motion and follow.
+                            Vector motion = hostEntity.getVelocity();
+                            double dX = MathHelper.a(motion.getX(), -3.9D, 3.9D) * 8000D; // I think this is a clamping function + multiply?
+                            double dY = MathHelper.a(motion.getY(), -3.9D, 3.9D) * 8000D;;
+                            double dZ = MathHelper.a(motion.getZ(), -3.9D, 3.9D) * 8000D;;
+                            NMS.setClassField(PacketPlayOutSpawnEntityLiving.class, addPacket, "g", (int) dX); // Velocity X
+                            NMS.setClassField(PacketPlayOutSpawnEntityLiving.class, addPacket, "h", (int) dY); // Location Y
+                            NMS.setClassField(PacketPlayOutSpawnEntityLiving.class, addPacket, "i", (int) dZ); // Location Z
+
+                            // Technically rotation doesn't matter but let's copy anyway.
+                            int yaw = (int) (hostEntity.getLocation().getYaw() * 256.0F / 360.0F);
+                            int pitch = (int) (hostEntity.getLocation().getPitch() * 256.0F / 360.0F);
+                            int head = (int) (((CraftLivingEntity)hostEntity).getHandle().getHeadRotation() * 256.0F / 360.0F); // Head rotation apparently.
+                            NMS.setClassField(PacketPlayOutSpawnEntityLiving.class, addPacket, "j", (byte) yaw); // Yaw
+                            NMS.setClassField(PacketPlayOutSpawnEntityLiving.class, addPacket, "k", (byte) pitch); // Pitch
+                            NMS.setClassField(PacketPlayOutSpawnEntityLiving.class, addPacket, "l", (byte) head); // Head rotate.
+
+                        } catch (NoSuchFieldException err) {
+                            OofTracker.getLog().severe("Error building packet. - No field! Is this the wrong version?");
+                            err.printStackTrace();
+                            visibleToPlayers.remove(p); // Remove player as it wasn't displayed.
+                            return;
+
+                        } catch (IllegalAccessException err) {
+                            OofTracker.getLog().severe("Error building packet - Can't access field! Is something misconfigured?");
+                            err.printStackTrace();
+                            visibleToPlayers.remove(p); // Remove player as it wasn't displayed.
+                            return;
+
+                        }
 
 
                         cPlayer.getHandle().playerConnection.sendPacket(addPacket);
