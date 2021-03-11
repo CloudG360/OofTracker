333c53a67c2dfbad009cb1145668cdfe556bceea
Will <willscullythewag@gmail.com>
2021-03-11 21:00:25 +0000

    Dead entities now remove their health bars

--- OofTracker-Spigot/src/main/java/net/cg360/spigot/ooftracker/indicator/HealthBarManager.java
+++ OofTracker-Spigot/src/main/java/net/cg360/spigot/ooftracker/indicator/HealthBarManager.java
@@ -7,6 +7,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.EntityDeathEvent;
 
 import java.util.HashMap;
 
@@ -37,13 +38,19 @@ public class HealthBarManager implements Listener {
     protected boolean checkTicks(int entityID) {
         long currentMilli = System.currentTimeMillis();  // Get current time now so it's consistent if needed.
 
-        long lastMilli = lastDamageMillis.get(entityID);
-        long delta = currentMilli - lastMilli; // Difference between then and now.
-        long maxConfigDelta = OofTracker.getConfiguration().get(ConfigKeys.HEALTH_BAR_VIEW_TICKS) * 50; // Tick is 0.050 seconds = 50 millis
+        // would be false if the entity died or something.
+        if(lastDamageMillis.containsKey(entityID)) {
+            long lastMilli = lastDamageMillis.get(entityID);
+            long delta = currentMilli - lastMilli; // Difference between then and now.
+            long maxConfigDelta = OofTracker.getConfiguration().get(ConfigKeys.HEALTH_BAR_VIEW_TICKS) * 50; // Tick is 0.050 seconds = 50 millis
 
-        // TRUE:    Delta is out of the bounds, remove the healthbar.
-        // FALSE:   Delta is in meaning a more recent damage occurred. Keep healthbar.
-        return delta < maxConfigDelta;
+            // TRUE:    Delta is out of the bounds, remove the healthbar.
+            // FALSE:   Delta is in meaning a more recent damage occurred. Keep healthbar.
+            return delta < maxConfigDelta;
+
+        } else {
+            return false;
+        }
     }
 
 
@@ -80,6 +87,20 @@ public class HealthBarManager implements Listener {
 
                 }, viewTicks + 1); // Ensure the delta will be past the max.
             }
+        }
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+    public void onEntityDestroy(EntityDeathEvent event) {
+        int entityID = event.getEntity().getEntityId();
+
+        // If true, invalidate and remove.
+        if(healthbars.containsKey(entityID)) {
+            LivingEntityHealthBar hb = healthbars.get(entityID);
+            hb.visible = false;
+            hb.update();
+
+            healthbars.remove(entityID);
         }
     }
 
