1d4dbc1bc74ca7115f8021571b869675735ec792
Will <willscullythewag@gmail.com>
2021-03-12 17:28:34 +0000

    #updatePositionAndVelocity() updates oldLocation to the new one.

--- OofTracker-Spigot/src/main/java/net/cg360/spigot/ooftracker/indicator/LivingEntityHealthBar.java
+++ OofTracker-Spigot/src/main/java/net/cg360/spigot/ooftracker/indicator/LivingEntityHealthBar.java
@@ -176,6 +176,7 @@ public class LivingEntityHealthBar {
 
                 this.visible = true;
                 updateDisplay(hostEntity.getHealth(), maxHealth == null ? 1d : maxHealth.getValue());
+                oldLocation = lastLocation;
             }
 
             for (Player player : visibleToPlayers) {
